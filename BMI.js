6f7f5fd9d24ddc9f23d06664c6de01545083b796
shamapumbu <88844339+shamapumbu@users.noreply.github.com>
2022-01-15 23:14:31 +0200

    Update BMI.js
    Added control statements to check bmi and inform the user whether they are underweight, normal weight or overweight.

--- BMI.js
+++ BMI.js
@@ -2,8 +2,26 @@ function bmi(weight, height) {
     var bmi = weight / Math.pow(height,2);
     return bmi;
 }
+function meaning(bodyMass) {
+    var underWeight = "Your body mass is " + bodyMass + ", so you are underweight.";
+    var overWeight = "Your body mass is " + bodyMass + ", so you are overweight.";
+    var normalWeight = "Your body mass is " + bodyMass + ", so have a normal weight.";
+    if(bodyMass < 18.5) {
+        return underWeight;
+    }
+    if(bodymass >= 18.5 && bodyMass <= 24.9) {
+        return normalWeight;
+    }
+    if(bodyMass > 24.9) {
+        return overWeight;
+    }
+}
 
 var myWeight = prompt("Enter your weight:");
 var myHeight = prompt("Enter your height");
 
+
+
 alert("Your BMI is: " + bmi(myWeight, myHeight));
+alert(meaning(bmi(myWeight,myHeight)));
+
